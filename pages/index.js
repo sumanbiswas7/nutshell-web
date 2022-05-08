56d2199f0314c31dc28e27f92fda795e67e807eb
suman <ronaldosuman97@gmail.com>
2022-05-09 00:02:47 +0530

    home mockup, bottom download btn added

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from "../styles/Home.module.scss";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { NavButton } from "../components/navbutton/NavButton";
+import { HiOutlineDownload } from "react-icons/hi";
 
 export default function Home() {
   return (
@@ -30,8 +31,30 @@ export default function Home() {
             <NavButton />
           </div>
         </nav>
-        <div className={styles.section_1}></div>
-        <div className={styles.section_2}></div>
+        <div className={styles.section_1}>
+          <div className={styles.download_section}>
+            <p>
+              We have created a dummy app, you can download it here your overall
+              app will look like this. But please remember that most of the
+              things like themes, logos, fonts are customizable. And depending
+              on the demand we will work on new templates as well.
+            </p>
+          </div>
+        </div>
+        <div className={styles.section_2}>
+          <img
+            src="/home/top_right_shape.svg"
+            className={styles.section_2__top_right_shape}
+          />
+          <img
+            src="/home/bottom_right_shape.svg"
+            className={styles.section_2__bottom_right_shape}
+          />
+          <img src="/home/phone_mockup.webp" className={styles.mockup} />
+          <div className={styles.download_btn}>
+            <HiOutlineDownload color="#FFF" size={30} />
+          </div>
+        </div>
       </main>
 
       <footer className={styles.footer}>
